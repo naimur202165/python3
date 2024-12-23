1b349bb6976ca9cee118e86d533faffebbff6226
naimur <naimur202165@gmail.com>
2024-12-23 19:53:50 +0600

    working with my list

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+import random
+
+
 print("heloo world")
 
 if(5>2):
@@ -14,3 +17,52 @@ y="Naimur Rahman Durjoy"
 
 
 print(x,y)
+# working with func
+
+
+h="naimur rahman"
+
+def func():
+    print("I am "+h)
+
+    func()
+
+
+def rony():
+    global x
+    x="This is a global variables"
+
+    rony()
+
+# Working with numbers
+
+
+num1=1
+num2=3
+com=1j
+print(num1,num2,com)
+print(random.randrange(1,10))
+
+
+
+
+name="naimurahman"
+
+
+print(name.lower())
+print(name.strip())
+# print(name.count())
+
+
+# working with lists
+
+
+mylist=["naimur","basic","ridoy"]
+
+
+print(mylist)
+
+print(len(mylist))
+
+
+
